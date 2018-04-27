b2e6508d7efd36cd6644942d798cebc0a904d6bc
queratg <gwendal.querat@gmail.com>
2018-04-27 16:16:10 +0100

    getModuleById() working

--- athome/api/schema.py
+++ athome/api/schema.py
@@ -1,13 +1,30 @@
 import  graphene
+import  graphene_django
 from    graphene_django import DjangoObjectType
-from    athome.api.models import Module
+from    athome.api.models import Module, Sample
+
 
 class ModuleType(DjangoObjectType):
     class Meta:
         model = Module
 
+class SampleType(DjangoObjectType):
+    class Meta:
+        model = Sample
+
+
 class Query(object):
-    allModules = graphene.List(ModuleType)
+    allModules      = graphene.List(ModuleType)
+    allSamples      = graphene.List(SampleType)
+    getModuleById   = graphene.Field(ModuleType, moduleId=graphene.Int())
 
     def resolve_allModules(self, info, **kwargs):
         return Module.objects.all()
+
+    def resolve_allSamples(self, info, **kwargs):
+        return Sample.objects.all()
+
+    def resolve_getModuleById(self, info, **kwargs):
+        return ModuleType.get_node(info, kwargs.get("moduleId"))
+
+
